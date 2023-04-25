e53efe9ddc7f1d46dd5e8c1be0fea31b0e97154f
João Paulo <jpcchaves@outlook.com>
2023-04-25 12:58:30 -0300

    feat: add mapper utils class

---
+++ src/main/java/com/jpcchaves/waiterapp/utils/mapper/MapperUtils.java
@@ -0,0 +1,32 @@
+package com.jpcchaves.waiterapp.utils.mapper;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class MapperUtils {
+    private final ModelMapper mapper;
+
+    public MapperUtils(ModelMapper mapper) {
+        this.mapper = mapper;
+    }
+
+    public <O, D> D parseObject(O origin, Class<D> destination) {
+        return mapper.map(origin, destination);
+    }
+
+    public <O, D> List<D> parseListObjects(List<O> origin, Class<D> destination) {
+
+        List<D> destinationObjects = new ArrayList<>();
+
+        for (O o : origin) {
+            destinationObjects.add(mapper.map(o, destination));
+        }
+
+        return destinationObjects;
+    }
+
+}
