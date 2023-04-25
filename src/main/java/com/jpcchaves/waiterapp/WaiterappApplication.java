9b86c5a0e3c3ccf7eb260ac62749da43e3cd34db
João Paulo <jpcchaves@outlook.com>
2023-04-25 12:46:33 -0300

    first commit

---
+++ src/main/java/com/jpcchaves/waiterapp/WaiterappApplication.java
@@ -0,0 +1,13 @@
+package com.jpcchaves.waiterapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaiterappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaiterappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jpcchaves/waiterapp/WaiterappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpcchaves.waiterapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaiterappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
