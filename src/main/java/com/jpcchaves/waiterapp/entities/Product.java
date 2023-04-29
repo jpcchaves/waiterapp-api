7c0ded48d3faf036e7f6086d414be56eeaae41da
João Paulo <jpcchaves@outlook.com>
2023-04-29 18:13:16 -0300

    feat: add product entity

---
+++ src/main/java/com/jpcchaves/waiterapp/entities/Product.java
@@ -0,0 +1,59 @@
+package com.jpcchaves.waiterapp.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String productName;
+    @Column(nullable = false)
+    private String productDescription;
+    @Column(nullable = false)
+    private Double price;
+
+    public Product() {
+    }
+
+    public Product(Long id, String productName, String productDescription, Double price) {
+        this.id = id;
+        this.productName = productName;
+        this.productDescription = productDescription;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getProductDescription() {
+        return productDescription;
+    }
+
+    public void setProductDescription(String productDescription) {
+        this.productDescription = productDescription;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+}
