cfa3327a4ae0b768a7cf84a50ad89676159cf0a0
João Paulo <jpcchaves@outlook.com>
2023-05-01 11:23:05 -0300

    refactor: adjustment in line item service (resolved not being calculated total)

--- src/main/java/com/jpcchaves/waiterapp/services/impl/LineItemServiceImpl.java
+++ src/main/java/com/jpcchaves/waiterapp/services/impl/LineItemServiceImpl.java
@@ -1,18 +1,16 @@
 package com.jpcchaves.waiterapp.services.impl;
 
 import com.jpcchaves.waiterapp.entities.LineItem;
-import com.jpcchaves.waiterapp.payload.dtos.lineitem.LineItemDto;
 import com.jpcchaves.waiterapp.entities.Order;
 import com.jpcchaves.waiterapp.entities.Product;
+import com.jpcchaves.waiterapp.payload.dtos.lineitem.LineItemDto;
 import com.jpcchaves.waiterapp.repositories.LineItemRepository;
 import com.jpcchaves.waiterapp.repositories.OrderRepository;
 import com.jpcchaves.waiterapp.repositories.ProductRepository;
 import com.jpcchaves.waiterapp.services.LineItemService;
-import org.aspectj.weaver.ast.Or;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class LineItemServiceImpl implements LineItemService {
@@ -31,27 +29,25 @@ public class LineItemServiceImpl implements LineItemService {
 
     @Override
     public String createLineItem(LineItemDto lineItem) {
-        Optional<Order> optionalOrder = Optional.ofNullable(orderRepository
+        Order order = orderRepository
                 .findById(lineItem.getOrderId())
-                .orElseThrow(() -> new RuntimeException("Order not found for the given id: " + lineItem.getOrderId())));
+                .orElseThrow(() -> new RuntimeException("Order not found for the given id: " + lineItem.getOrderId()));
 
-        Optional<Product> optionalProduct = Optional.ofNullable(productRepository
+        Product product = productRepository
                 .findById(lineItem.getProductId())
-                .orElseThrow(() -> new RuntimeException("Order not found for the given id: " + lineItem.getOrderId())));
+                .orElseThrow(() -> new RuntimeException("Order not found for the given id: " + lineItem.getOrderId()));
 
         LineItem newItem = new LineItem();
 
-        if (optionalOrder.isPresent() && optionalProduct.isPresent()) {
-            Order order = optionalOrder.get();
-            Product product = optionalProduct.get();
-            newItem.setSubTotal(calculateSubTotal(lineItem.getQuantity(), product.getPrice()));
-            newItem.setOrder(order);
-            newItem.setProduct(product);
-            newItem.setQuantity(lineItem.getQuantity());
+        newItem.setSubTotal(calculateSubTotal(lineItem.getQuantity(), product.getPrice()));
+        newItem.setOrder(order);
+        newItem.setProduct(product);
+        newItem.setQuantity(lineItem.getQuantity());
 
-            Double total = calculateOrderTotal(order.getLineItems());
-            order.setOrderTotal(total);
-        }
+        order.getLineItems().add(newItem);
+
+        Double total = calculateOrderTotal(order.getLineItems());
+        order.setOrderTotal(total);
 
         repository.save(newItem);
         return "Item successfully added to the order";
@@ -66,7 +62,6 @@ public class LineItemServiceImpl implements LineItemService {
         for (LineItem lineItem : lineItems) {
             total += lineItem.getSubTotal();
         }
-
         return total;
     }
 }
