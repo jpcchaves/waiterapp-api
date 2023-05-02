ca73533de2bd5960b3f9640ff3d2bf6d71cb432b
João Paulo <jpcchaves@outlook.com>
2023-05-02 12:01:28 -0300

    feat: add migration create table products

---
+++ src/main/resources/db/migration/V2__Create_Table_Orders.sql
@@ -0,0 +1,1 @@
+create table orders (order_id bigint not null auto_increment, conclusion_date datetime(6), created_at datetime(6), is_done bit, is_paid bit, order_code binary(16), order_details varchar(255), order_total float(53), status smallint, primary key (order_id));
