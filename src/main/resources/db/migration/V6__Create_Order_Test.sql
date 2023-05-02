8e305a0be1b97599aa1701a9f4960743f0f8c273
João Paulo <jpcchaves@outlook.com>
2023-05-02 12:03:31 -0300

    feat: add migration create test order

---
+++ src/main/resources/db/migration/V6__Create_Order_Test.sql
@@ -0,0 +1,4 @@
+INSERT INTO `waiterapp`.`orders`
+    (`order_id`, `conclusion_date`, `created_at`, `is_done`, `is_paid`, `order_code`,`order_details`, `order_total`, `status`)
+VALUES
+    ('1', NULL, '2023-05-02 00:00:00', 0, 0, 1000001010101010, "Pedido de Teste", 0,  0);
