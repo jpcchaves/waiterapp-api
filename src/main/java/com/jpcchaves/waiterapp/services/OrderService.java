14fd18917552e9b4e96303fe2529d8db5def9323
João Paulo <jpcchaves@outlook.com>
2023-05-03 13:14:06 -0300

    feat: add order create usecase

--- src/main/java/com/jpcchaves/waiterapp/services/OrderService.java
+++ src/main/java/com/jpcchaves/waiterapp/services/OrderService.java
@@ -1,9 +1,12 @@
 package com.jpcchaves.waiterapp.services;
 
+import com.jpcchaves.waiterapp.payload.dtos.order.OrderRequestDto;
 import com.jpcchaves.waiterapp.payload.dtos.order.OrderResponseDto;
 
 import java.util.List;
 
 public interface OrderService {
     List<OrderResponseDto> getAll();
+
+    OrderResponseDto create(OrderRequestDto orderRequestDto);
 }
