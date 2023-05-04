ce2a251ab651f236ddd832214cf648be2808c389
João Paulo <jpcchaves@outlook.com>
2023-05-04 13:01:22 -0300

    feat: impl int identifiers to OrderStatus enum

--- src/main/java/com/jpcchaves/waiterapp/Enum/OrderStatus.java
+++ src/main/java/com/jpcchaves/waiterapp/Enum/OrderStatus.java
@@ -1,7 +1,27 @@
 package com.jpcchaves.waiterapp.Enum;
 
 public enum OrderStatus {
-    WAITING,
-    IN_PROGRESS,
-    DONE;
+    WAITING(0),
+    IN_PROGRESS(1),
+    DONE(2);
+
+    private final int code;
+
+    OrderStatus(int code) {
+        this.code = code;
+    }
+
+    public static OrderStatus valueOf(int code) {
+        for (OrderStatus value : OrderStatus.values()) {
+            if (code == value.getCode()) {
+                return value;
+            }
+        }
+        throw new IllegalArgumentException("Invalid code: " + code);
+    }
+
+    public int getCode() {
+        return code;
+    }
+
 }
