c24cead948bff28142ef8a7fa1b22aa33bac5e73
João Paulo <jpcchaves@outlook.com>
2023-05-05 11:08:40 -0300

    feat: add new jpa operation to list all line items by product id

--- src/main/java/com/jpcchaves/waiterapp/repositories/LineItemRepository.java
+++ src/main/java/com/jpcchaves/waiterapp/repositories/LineItemRepository.java
@@ -3,5 +3,8 @@ package com.jpcchaves.waiterapp.repositories;
 import com.jpcchaves.waiterapp.entities.LineItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface LineItemRepository extends JpaRepository<LineItem, Long> {
+    List<LineItem> getLineItemsByProduct_Id(Long productId);
 }
