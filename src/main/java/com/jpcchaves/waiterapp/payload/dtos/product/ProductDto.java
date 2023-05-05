0691c6d9a10cfd5dece784250d79fa26468b0938
João Paulo <jpcchaves@outlook.com>
2023-05-05 10:09:07 -0300

    refactor: change product response dto to only product dto

--- src/main/java/com/jpcchaves/waiterapp/payload/dtos/product/ProductDto.java
+++ src/main/java/com/jpcchaves/waiterapp/payload/dtos/product/ProductDto.java
@@ -1,18 +1,18 @@
 package com.jpcchaves.waiterapp.payload.dtos.product;
 
-public class ProductResponseDto {
+public class ProductDto {
     private Long id;
     private String name;
     private String description;
     private Double price;
 
-    public ProductResponseDto() {
+    public ProductDto() {
     }
 
-    public ProductResponseDto(Long id,
-                              String name,
-                              String description,
-                              Double price) {
+    public ProductDto(Long id,
+                      String name,
+                      String description,
+                      Double price) {
         this.id = id;
         this.name = name;
         this.description = description;
